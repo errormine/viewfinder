06ff59d0fae954684a35b5f09f04b1db91570ddb
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-14 10:05:09 -0600

    Update MariaDB setup script***

--- build/packer/scripts/team02m/post_install_mariadb_setup.sh
+++ build/packer/scripts/team02m/post_install_mariadb_setup.sh
@@ -2,13 +2,30 @@
 
 # Install MariaDB
 sudo apt-get update
+curl -LsS https://r.mariadb.com/downloads/mariadb_repo_setup | sudo bash
 sudo apt-get install -y mariadb-server
+mariadb -V
 
 # Start MariaDB service
 sudo systemctl start mariadb
 
 # Create tables from team02m_db.sql
-sudo mysql < ./team02m_db.sql
+sudo mariadb < ./team02m_db.sql
+
+# Change port number
+sudo sed -i 's/^port\s*=.*/port = 3307/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# Allow external connections
+sudo sed -i 's/^bind-address\s*=.*/bind-address = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
 
 # Restart MariaDB service
 sudo systemctl restart mariadb
+
+# Set root password
+#mariadb -u root <<EOF
+#SET PASSWORD FOR root@localhost = '${ROOT_PASSWORD}';
+#FLUSH PRIVILEGES;
+#EOF
+
+# Secure MariaDB
+#mariadb_secure_installation -u root --password="${ROOT_PASSWORD}" --use-default

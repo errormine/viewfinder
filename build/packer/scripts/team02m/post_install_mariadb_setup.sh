a2e1f783857705c9b768a11eba3c07434109e026
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-14 11:10:30 -0600

    Make DB user accounts in setup script

--- build/packer/scripts/team02m/post_install_mariadb_setup.sh
+++ build/packer/scripts/team02m/post_install_mariadb_setup.sh
@@ -23,14 +23,16 @@ sudo systemctl start firewalld
 sudo firewall-cmd --zone=meta-network --add-port=3307/tcp --permanent
 sudo firewall-cmd --reload
 
+# Create admin user
+sudo mariadb -e "CREATE USER '${DBADMIN}'@'localhost' IDENTIFIED BY 'password';"
+sudo mariadb -e "SET PASSWORD FOR '${DBADMIN}'@'localhost' = PASSWORD('${DBADMINPASSWORD}');"
+sudo mariadb -e "GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO '${DBADMIN}'@'localhost';"
+
+# Create user for web app
+sudo mariadb -e "CREATE USER '${DBUSER}'@'localhost' IDENTIFIED BY 'password';"
+sudo mariadb -e "SET PASSWORD FOR '${DBUSER}'@'localhost' = PASSWORD('${DBUSERPASSWORD}');"
+sudo mariadb -e "GRANT SELECT ON team02_db.* TO '${DBUSER}'@'localhost';"
+# THIS IS BAD!!! MAKE IT SECURE
+
 # Restart MariaDB service
 sudo systemctl restart mariadb
-
-# Set root password
-#mariadb -u root <<EOF
-#SET PASSWORD FOR root@localhost = '${ROOT_PASSWORD}';
-#FLUSH PRIVILEGES;
-#EOF
-
-# Secure MariaDB
-#mariadb_secure_installation -u root --password="${ROOT_PASSWORD}" --use-default

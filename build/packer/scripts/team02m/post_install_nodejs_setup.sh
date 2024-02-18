ae7dfed11aedf36eee0e82ecbc1f1dbaa7f6ed6a
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-18 17:03:55 -0600

    Add post install script for node js infra

---
+++ build/packer/scripts/team02m/post_install_nodejs_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Clone the repository
+git clone git@github.com:illinoistech-itm/team02m-2024.git
+
+# Install Node.js
+sudo apt update
+curl -fsSL https://deb.nodesource.com/setup_21.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs
+
+# Install dependencies
+sudo npm install -g express pm2
+
+# Open firewalld
+sudo firewall-cmd --zone=public --add-port=5000/tcp --permanent
+sudo firewall-cmd --reload
+
+# Start the server
+cd team02m-2024/code/nodejs
+pm2 start index.js

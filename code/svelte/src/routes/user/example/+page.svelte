476a719acd693416aef889715423d72440497f37
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-28 09:17:51 -0600

    Fix user about page padding

--- code/svelte/src/routes/user/example/+page.svelte
+++ code/svelte/src/routes/user/example/+page.svelte
@@ -1,9 +1,17 @@
-<main id="about" class="content-grid">
-    <p>Here is the user bio I guess</p>
+<main class="content-grid">
+    <section class="about-section">
+        <p>Here is the user bio I guess</p>
+    </section>
 </main>
 
 <style>
-    #about {
+    main {
+        background: var(--color-off-white);
         grid-column: content;
+    }
+
+    .about-section {
+        background: white;
+        padding: 1.5rem 4rem;
     }
 </style>

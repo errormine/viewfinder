af8f32b5d27ccc6b86abd140d8a8ea54d9b6e1df
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-27 18:14:31 -0600

    Fix layout of user profile hero

--- code/svelte/src/routes/user/+layout.svelte
+++ code/svelte/src/routes/user/+layout.svelte
@@ -1,20 +1,20 @@
-<main class="content-grid">
-    <header class='hero full-width'>
-        <img src="https://picsum.photos/200" alt="">
-        <h2>British Library</h2>
-        <p class="tagline">@thebritishlibrary</p>
-        <ul>
-            <li>
-                <p><span>1.5K</span> Photos</p>
-            </li>
-            <li>
-                <p><span>60.5K</span> Followers</p>
-            </li>
-            <li>
-                <p><span>30</span> Following</p>
-            </li>
+<header class='hero full-width'>
+    <section class='user-info'>
+        <figure class='user-portrait'>
+            <img src="https://picsum.photos/200" alt="">
+            <figcaption>
+                <h2 class='user-display-name'>British Library</h2>
+                <p class="user-handle font-weight-light">@thebritishlibrary</p>
+            </figcaption>
+        </figure>
+        <ul class='user-stats'>
+            <li><span class='font-weight-semi-bold'>1.5K</span> Photos</li>
+            <li><span class='font-weight-semi-bold'>60.5K</span> Followers</li>
+            <li><span class='font-weight-semi-bold'>30</span> Following</li>
         </ul>
-    </header>
+    </section>
+</header>
+<main class="content-grid">
     <nav class='profile-navigation full-width'>
         <ul>
             <li><a href="/user/[username]">About</a></li>
@@ -29,7 +29,7 @@
 <style>
     :global(main), .content-grid, .full-width {
         display: grid;
-        grid-template-columns: [full-width-start] 1fr [content-start] minmax(0, 64rem) [content-end] 1fr [full-width-end];
+        grid-template-columns: [full-width-start] 1fr [content-start] minmax(0, 50rem) [content-end] 1fr [full-width-end];
     }
 
     .content-grid > *,
@@ -42,10 +42,57 @@
     }
 
     .hero {
-        background: url(https://picsum.photos/1920/1080) no-repeat center;
+        color: white;
+        background: var(--transparent-black-gradient), url(https://picsum.photos/1920/1080) no-repeat center;
         background-size: cover;
         position: relative;
-        max-height: 20rem;
+        height: 20rem;
+    }
+    
+    .user-info {
+        width: 100%;
+        position: absolute;
+        bottom: 1.2rem;
+    }
+    
+    .user-portrait {
+        display: block;
+        gap: 1.5rem;
+    }
+
+    .user-portrait img {
+        float: left;
+        border-radius: 50%;
+        width: 7rem;
+        height: 7rem;
+        border: 2px white solid;
+        margin-right: 1.5rem;
+    }
+
+    .user-portrait figcaption {
+        display: inline-block;
+        position: relative;
+        top: 1.25rem;
+    }
+
+    .user-display-name {
+        font-size: 2.75rem;
+        font-weight: 400;
+        margin: 0;
+    }
+
+    .user-handle {
+        margin: 0;
+    }
+
+    .user-stats {
+        float: right;
+        position: relative;
+        bottom: 1.5rem;
+        display: flex;
+        gap: 0.25rem;
+        flex-direction: column;
+        text-align: right;
     }
 
     .profile-navigation {

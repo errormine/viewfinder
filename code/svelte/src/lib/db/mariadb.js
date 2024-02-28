eb1f41697c389d8cd95586723593ac8843ce3e97
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-28 17:59:10 -0600

    Write functions to retrieve userId and displayName

--- code/svelte/src/lib/db/mariadb.js
+++ code/svelte/src/lib/db/mariadb.js
@@ -2,7 +2,8 @@ import mariadb from 'mariadb';
 const pool = mariadb.createPool({
     host: import.meta.env.VITE_DB_HOST,
     user: import.meta.env.VITE_DB_USER,
-    password: import.meta.env.VITE_DB_PASS
+    password: import.meta.env.VITE_DB_PASS,
+    database: "team02m_db"
 });
 
 console.log(`DB_HOST: ${import.meta.env.VITE_DB_HOST}, DB_USER: ${import.meta.env.VITE_DB_USER}`);
@@ -17,5 +18,42 @@ export async function testConnection() {
         })
         .catch(err => {
             return err;
+        });
+}
+
+async function performQuery(query, param) {
+    let conn = await pool.getConnection();
+
+    return conn.query(query, param)
+        .then(rows => {
+            return rows;
         })
+        .catch(err => {
+            return err;
+        });
+}
+
+async function getSingleRow(query, param) {
+    return performQuery(query, param)
+        .then(rows => {
+            return rows[0];
+        });
+}
+
+async function getSingleValue(query, param) {
+    return getSingleRow(query, param)
+        .then(res => {
+            return Object.values(res)[0];
+        })
+        .catch(err => {
+            return null;
+        });
+}
+
+export async function getUserId(username) {
+    return getSingleValue("SELECT UserId FROM Users WHERE Username = ?", [username]);
+}
+
+export async function getDisplayName(userId) {
+    return getSingleValue("SELECT DisplayName FROM Users WHERE UserId = ?", [userId])
 }
--- code/svelte/src/routes/api/user/[username]/+server.js
+++ code/svelte/src/routes/api/user/[username]/+server.js
@@ -2,7 +2,8 @@ import { json } from '@sveltejs/kit';
 import * as database from '$lib/db/mariadb';
 
 export const GET = async ({ params, request }) => {
-    let result = await database.testConnection();
+    let userId = await database.getUserId(params.username);
+    let displayName = await database.getDisplayName(userId);
 
-    return json(result);
+    return json({ userId, displayName });
 }

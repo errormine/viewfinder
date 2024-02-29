16e82e118c7374a791a712403f5733a8da94de0e
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-29 12:01:50 -0600

    Move some user profile styles to a common css file

--- code/svelte/src/routes/user/[username]/+page.svelte
+++ code/svelte/src/routes/user/[username]/+page.svelte
@@ -1,25 +1,35 @@
 <script>
     import ProfileBubble from "$lib/components/ProfileBubble.svelte";
+
+    /** @type {import('./$types').LayoutData} */
+    export let data;
 </script>
 
-<section class="profile-bubbles">
+<section id="profile-bubbles">
     <ProfileBubble label="Website" href="https://example.com" />
     <ProfileBubble label="Contact" href="mailto:contact@example.com" />
 </section>
-<section class="user-bio">
+<section id="user-bio">
     <p>Here is the user bio I guess</p>
 </section>
-<section class="user-showcase">
-    <h3>Showcase</h3>
+<section id="user-showcase" >
+    <header>
+        <h3>Showcase</h3>
+    </header>
     <p>Here is the user showcase</p>
 </section>
-<section class="user-recent-photos">
-    <h3>Recent Photos</h3>
+<section id="user-recent-photos">
+    <header>
+        <h3>Recent Photos</h3>
+        <a href="/user/{ data.username }/photos">View more</a>
+    </header>
     <p>Here are the user's recent photos</p>
 </section>
 
 <style>
-    .profile-bubbles {
+    @import url('/styles/user-profile.css');
+
+    #profile-bubbles {
         display: grid;
         grid-template-columns: 1fr 1fr;
         gap: 1rem;

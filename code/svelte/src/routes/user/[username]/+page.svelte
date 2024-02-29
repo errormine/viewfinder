6bc7c3711cf4512fbe48ea054e22245d4a7ee236
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-02-29 10:56:10 -0600

    Add showcase and recent photos section to profile

--- code/svelte/src/routes/user/[username]/+page.svelte
+++ code/svelte/src/routes/user/[username]/+page.svelte
@@ -8,6 +8,15 @@
 </section>
 <section class="user-bio">
     <p>Here is the user bio I guess</p>
+</section>
+<section class="user-showcase">
+    <h3>Showcase</h3>
+    <p>Here is the user showcase</p>
+</section>
+<section class="user-recent-photos">
+    <h3>Recent Photos</h3>
+    <p>Here are the user's recent photos</p>
+</section>
 
 <style>
     .profile-bubbles {

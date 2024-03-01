e222cd4ecdf5c7d06d344ec7df340da89f0cf1f4
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-01 17:11:39 -0600

    Add IconButton component

---
+++ code/svelte/src/lib/components/IconButton.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let title = '';
+    export let showBG = false;
+</script>
+
+<!-- You have to forward on:click here or the button won't work! -->
+<!-- https://svelte.dev/tutorial/dom-event-forwarding -->
+
+<button on:click {title} class="round-corners" class:show-bg={showBG}>
+    <slot />
+</button>
+
+<style>
+    button {
+        display: block;
+        cursor: pointer;
+        padding: 0.5rem;
+        transition: 200ms;
+        background-color: transparent;
+        border: 1px solid transparent;
+        height: 2rem;
+        width: 2rem;
+    }
+
+    .show-bg,
+    button:hover,
+    button:focus {
+        background-color: var(--color-off-white);
+        border: 1px solid var(--color-gray);
+    }
+
+    button:active {
+        background-color: var(--color-light-gray);
+    }
+</style>

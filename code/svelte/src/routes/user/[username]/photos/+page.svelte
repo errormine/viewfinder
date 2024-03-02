bf24c89b3dc7dd617c62caafaa745d9170fbb6cf
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-02 11:48:57 -0600

    Fill out user profile photo gallery more

--- code/svelte/src/routes/user/[username]/photos/+page.svelte
+++ code/svelte/src/routes/user/[username]/photos/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     import PhotoGrid from '$lib/components/PhotoGrid.svelte';
+    import IconButton from '$lib/components/IconButton.svelte';
+	import { SortAsc16, SortDesc16 } from 'svelte-octicons';
 
     /** @type {import('./$types').PageData} */
     export let data;
@@ -8,6 +10,13 @@
 <section id="photo-gallery">
     <header>
         <h3>Photo Gallery</h3>
+        <IconButton title="Sort: Newest" noBG>
+            <SortDesc16 />
+        </IconButton>
     </header>
     <PhotoGrid photos={data.photos} />
 </section>
+
+<style>
+    @import url('/styles/user-profile.css');
+</style>

44594e4267043c91256b8376196dce181664676a
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-02 19:21:16 -0600

    Create album components

---
+++ code/svelte/src/lib/components/AlbumFigure.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let album;
+</script>
+
+<figure class="round-corners">
+    <img src={album.Thumbnail} alt="" />
+    <figcaption>
+        <a href="/" class="font-weight-semi-bold" title={album.Name}>{album.Name}</a>
+        <small>{album.Count} Photos</small>
+    </figcaption>
+</figure>
+
+<style>
+    @import url('/styles/user-profile.css');
+
+    figure {
+        overflow: hidden;
+    }
+    
+    figcaption {
+        background: var(--color-gray-gradient);
+        padding: 0.5rem;
+        height: 3rem;
+    }
+    
+    figcaption a {
+        display: block;
+        text-decoration: none;
+        color: black;
+        font-size: 1.2rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+    }
+    </style>
---
+++ code/svelte/src/lib/components/AlbumGrid.svelte
@@ -0,0 +1,19 @@
+<script>
+    import AlbumFigure from "$lib/components/AlbumFigure.svelte";
+
+    export let albums;
+</script>
+
+<section class="album-grid">
+    {#each albums as album}
+        <AlbumFigure {album} />
+    {/each}
+</section>
+
+<style>
+    .album-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 0.5rem;
+    }
+</style>

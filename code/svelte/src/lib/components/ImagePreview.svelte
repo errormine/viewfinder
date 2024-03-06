f44f2c99eedd01545aa8219609759a700dc42934
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-06 14:01:45 -0600

    Add ImagePreview component

---
+++ code/svelte/src/lib/components/ImagePreview.svelte
@@ -0,0 +1,20 @@
+<!-- Lets you access props when the component is created programatically -->
+<svelte:options accessors />
+
+<script>
+    export let _element;
+    export let src = "";
+    export let onClick = () => {};
+</script>
+
+<button bind:this={_element} on:click={onClick}>
+    <img {src} alt="">
+</button>
+
+<style>
+    img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+    }
+</style>

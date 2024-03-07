f2a7bf7bd2a15be231441ea05921d212349ff497
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-06 20:57:33 -0600

    Add basic login handling in the header

--- code/svelte/src/lib/components/TopBar.svelte
+++ code/svelte/src/lib/components/TopBar.svelte
@@ -1,6 +1,12 @@
 <script>
-    import SearchBar from '$lib/components/SearchBar.svelte'
-    import Button from '$lib/components/Button.svelte'
+    import { signIn, signOut } from '@auth/sveltekit/client';
+    import { page } from '$app/stores';
+    import { Upload16 } from 'svelte-octicons';
+    import SearchBar from '$lib/components/SearchBar.svelte';
+    import ActionBar from '$lib/components/ActionBar.svelte';
+    import Button from '$lib/components/Button.svelte';
+    import IconButton from '$lib/components/IconButton.svelte';
+    import UserPortrait from '$lib/components/UserPortrait.svelte';
 
     let dialog;
 </script>
@@ -10,13 +16,25 @@
         <button class="close-button" on:click={dialog.close()}>X</button>
         <h2>Join Website</h2>
         <p>Create an account to post photos, comment, and save photos from others.</p>
-        <Button href="/auth/google">Log in with Google</Button>
+        <Button on:click={() => signIn("google")}>Log in with Google</Button>
     </section>
 </dialog>
 <header>
     <h1><a href="/">Website</a></h1>
     <SearchBar />
-    <Button on:click={dialog.showModal()} align={"right"}>Log In</Button>
+    {#if $page.data.session}
+        <ul>
+            <li><button on:click={() => signOut()}>Sign Out</button></li>
+        </ul>
+        <ActionBar>
+            <IconButton href="/upload" shape={"circle"}>
+                <Upload16 title={"Upload"}/>
+            </IconButton>
+            <UserPortrait src={$page.data.session.user.image} size={2} color={"gray"}/>
+        </ActionBar>
+    {:else}
+        <Button on:click={dialog.showModal()} align={"right"}>Log In</Button>
+    {/if}
 </header>
 
 <style>

228c1e985fe51cb476964b9ddba228cd076bd159
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-08 20:03:32 -0600

    Fix null bio throwing error

--- code/svelte/src/routes/user/[username]/+page.server.js
+++ code/svelte/src/routes/user/[username]/+page.server.js
@@ -4,13 +4,14 @@ import * as db from '$lib/server/mariadb';
 export async function load({ parent }) {
     // Waits for +layout.server.js to finish loading
     const { userId } = await parent();
+    let bio = await db.getBio(userId);
 
     return {
         website: await db.getWebsite(userId),
         contact: await db.getContact(userId),
         location: await db.getLocation(userId),
         joinDate: await db.getJoinDate(userId),
-        bio: (await db.getBio(userId)).replace(/<[^>]*>/g, ''),
+        bio: bio == null ? 'No information provided.' : bio.replace(/<[^>]*>/g, ''),
         recentPhotos: await db.getRecentPhotos(userId)
     };
 };

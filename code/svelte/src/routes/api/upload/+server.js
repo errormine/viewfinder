db4755fe5f31238fcbce4ad78e8d10025c363141
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-08 19:34:38 -0600

    Validate POST requests to upload api

--- code/svelte/src/routes/api/upload/+server.js
+++ code/svelte/src/routes/api/upload/+server.js
@@ -1,8 +1,19 @@
 import * as db from '$lib/server/mariadb';
+import { lucia } from '$lib/server/auth';
 
 /** @type {import('./$types').RequestHandler} */
-export async function POST({ request }) {
+export async function POST({ request, cookies }) {
     const { photo } = await request.json();
+    const session = cookies.get('auth_session');
+    console.log(session);
+
+    if (!session) {
+        return new Response(JSON.stringify({ error: "Not authenticated."}), { status: 401 });
+    }
+
+    if (!lucia.validateSession(session)) {
+        return new Response(JSON.stringify({ error: "Invalid session."}), { status: 401 });
+    }
 
     if (photo) {
         return db.uploadPhoto(1, photo)

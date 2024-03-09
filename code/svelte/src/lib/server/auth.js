b50556f1d5af34fc8da6353ae2f389aef142112d
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-09 11:04:50 -0600

    Set the redirect uri dynamically

--- code/svelte/src/lib/server/auth.js
+++ code/svelte/src/lib/server/auth.js
@@ -3,6 +3,7 @@ import { Lucia } from 'lucia';
 import { dev } from '$app/environment';
 import { MariaDBAdapter } from '$lib/adapters/mariadb';
 import { Google } from 'arctic';
+import { dirname } from '@sveltejs/kit';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 const adapter = new MariaDBAdapter();
@@ -15,9 +16,12 @@ export const lucia = new Lucia(adapter, {
     }
 });
 
+const baseUrl = import.meta.env.DEV ? `http://localhost:5173` : `${dirname(import.meta.url)}`;
+const redirectUri = `${baseUrl}/auth/google/callback`;
+
 export const google = new Google(
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
-    `http://localhost:5173/auth/google/callback`
+    redirectUri
 );
 

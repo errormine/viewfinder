b08330b37fe995507e142692b139ee8f0bbf8477
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-21 23:00:35 -0500

    Change images api parameter to lowercase uuid

--- code/svelte/src/routes/api/images/[uuid]/+server.js
+++ code/svelte/src/routes/api/images/[uuid]/+server.js
@@ -3,7 +3,7 @@ import { S3_BUCKET } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ params }) {
-    const image = await minioClient.getObject(S3_BUCKET, params.UUID);
+    const image = await minioClient.getObject(S3_BUCKET, params.uuid);
 
     return new Response(image);
 };

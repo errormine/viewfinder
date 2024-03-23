651163a19fd13e17da484059ae4146e826f7e454
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-23 17:18:02 -0500

    Return user object in layout instead of per page

--- code/svelte/src/routes/upload/+page.server.js
+++ code/svelte/src/routes/upload/+page.server.js
@@ -3,8 +3,4 @@ import { redirect } from "@sveltejs/kit";
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ locals }) {
     if (!locals.user) redirect(302, "/");
-
-    return {
-        user: locals.user,
-    };
 };

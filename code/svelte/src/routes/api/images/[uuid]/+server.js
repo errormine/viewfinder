a3dc0200d0e93769cadae7eef721dd9078e75cdf
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-24 14:34:33 -0500

    Change S3_BUCKET variable -> S3_BUCKET_NAME

--- code/svelte/src/routes/api/images/[uuid]/+server.js
+++ code/svelte/src/routes/api/images/[uuid]/+server.js
@@ -1,9 +1,9 @@
 import { minioClient } from '$lib/server/minio';
-import { S3_BUCKET } from '$env/static/private';
+import { S3_BUCKET_NAME } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({ params }) {
-    const image = await minioClient.getObject(S3_BUCKET, params.uuid);
+    const image = await minioClient.getObject(S3_BUCKET_NAME, params.uuid);
 
     return new Response(image);
 };

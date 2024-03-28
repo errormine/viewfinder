9834bc5d69767b71542a105f1faa7ce9174077f1
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-03-28 15:36:37 -0500

    Add styles for settings page

--- code/svelte/src/routes/settings/+page.svelte
+++ code/svelte/src/routes/settings/+page.svelte
@@ -8,15 +8,17 @@
 <main class="content-grid">
     <section class="content">
         <h2>Settings</h2>
-        <section class="settings-meu">
-            <nav>
-                <ul>
-                    <li><a href="/settings#account">Account</a></li>
-                    <li><a href="/settings#profile">Profile</a></li>
-                    <li><a href="/settings#notifications">Notifications</a></li>
-                    <li><a href="/settings#privacy">Privacy</a></li>
-                </ul>
-            </nav>
+        <section class="settings-menu round-corners">
+            <section class="settings-left-bar">
+                <nav>
+                    <ul>
+                        <li><a href="/settings#account">Account</a></li>
+                        <li><a href="/settings#profile">Profile</a></li>
+                        <li><a href="/settings#notifications">Notifications</a></li>
+                        <li><a href="/settings#privacy">Privacy</a></li>
+                    </ul>
+                </nav>
+            </section>
             <section class="settings-main">
                 <section id="account">
                     <h3>Account</h3>
@@ -30,14 +32,16 @@
                         <label for="email">Email</label>
                         <input type="email" id="email" name="email" value="{data.user.email}">
 
-                        <Button type="submit">Save</Button>
+                        <footer>
+                            <Button type="submit" align={'right'}>Save</Button>
+                        </footer>
                     </form>
                 </section>
                 <section id="profile">
                     <h3>Profile</h3>
                     <form action="">
                         <label for="website">Website</label>
-                        <input type="url" id="website" name="website" value="{data.user.website}">
+                        <input type="text" id="website" name="website" value="{data.user.website}">
                         
                         <label for="location">Location</label>
                         <input type="text" id="location" name="location" value="{data.user.location}">
@@ -48,7 +52,9 @@
                         <label for="show-buttons">Show buttons on profile</label>
                         <input type="checkbox" id="show-buttons" name="show-buttons" checked="{data.user.showButtons}">
 
-                        <Button>Save</Button>
+                        <footer>
+                            <Button type="submit" align={'right'}>Save</Button>
+                        </footer>
                     </form>
                 </section>
                 <section id="notifications">
@@ -60,7 +66,9 @@
                         <label for="push-notifications">Push Notifications</label>
                         <input type="checkbox" id="push-notifications" name="push-notifications" checked="{data.user.pushNotifications}">
 
-                        <Button>Save</Button>
+                        <footer>
+                            <Button type="submit" align={'right'}>Save</Button>
+                        </footer>
                     </form>
                 </section>
                 <section id="privacy">
@@ -69,7 +77,9 @@
                         <label for="private-account">Private Account</label>
                         <input type="checkbox" id="private-account" name="private-account" checked="{data.user.privateAccount}">
 
-                        <Button>Save</Button>
+                        <footer>
+                            <Button type="submit" align={'right'}>Save</Button>
+                        </footer>
                     </form>
                 </section>
             </section>
@@ -80,5 +90,54 @@
 <style>
     .content {
         margin-top: 2rem;
+    }
+
+    .settings-menu {
+        display: grid;
+        grid-template-columns: 1fr 40rem;
+        gap: 1rem;
+        background-color: white;
+        margin-bottom: 2rem;
+        border: 1px solid var(--color-gray);
+    }
+
+    .settings-left-bar {
+        background: var(--color-gray-gradient);
+        border-radius: 0.5rem 0 0 0.5rem;
+        border-right: 1px solid var(--color-gray);
+    }
+
+    .settings-left-bar li {
+        display: block;
+        padding: 0.75rem;
+        padding-right: 1.25rem;
+        text-align: right;
+        border-bottom: 1px solid var(--color-gray);
+    }
+
+    .settings-left-bar a {
+        color: black;
+        text-decoration: none;
+        font-size: 1.5rem;
+        font-weight: 600;
+    }
+
+    .settings-left-bar nav {
+        position: sticky;
+        top: var(--header-height);
+    }
+
+    .settings-main {
+        padding: 1rem 1.5rem;
+    }
+
+    .settings-main form {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 1rem;
+    }
+
+    .settings-main form footer {
+        grid-column: span 2;
     }
 </style>

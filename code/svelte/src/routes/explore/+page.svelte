a7f9ca0efb5a97ad96f649e6aaa06cca5176c195
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-04-03 15:19:09 -0500

    Add skeleton for explore page

---
+++ code/svelte/src/routes/explore/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    /** @type {import('./$types').PageData} */
+    export let data;
+</script>
+
+<main>
+    <section class="explore-page">
+        <h2>Explore</h2>
+        <p>Explore photos from other users.</p>
+    </section>
+</main>
+
+<style>
+    .explore-page {
+        padding: 1rem;
+    }
+</style>

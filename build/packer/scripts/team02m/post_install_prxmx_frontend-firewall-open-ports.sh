fbc54bb85703845217f5043cb41abcb71cc6703f
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-04-08 16:35:09 -0500

    Fix firewall ports on frontend install script

--- build/packer/scripts/team02m/post_install_prxmx_frontend-firewall-open-ports.sh
+++ build/packer/scripts/team02m/post_install_prxmx_frontend-firewall-open-ports.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo firewall-cmd --zone=public --add-port=80/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=443/tcp --permanent
-sudo firewall-cmd --zone=public --add-port=3000/tcp --permanent
+sudo firewall-cmd --zone=meta-network --add-port=3000/tcp --permanent
 
 sudo firewall-cmd --reload

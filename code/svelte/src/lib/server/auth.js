e134de3d8bab5422e850d82aea2fbb915e302a33
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-04-09 18:55:03 -0500

    Add picture attribute to lucia user data

--- code/svelte/src/lib/server/auth.js
+++ code/svelte/src/lib/server/auth.js
@@ -19,6 +19,7 @@ export const lucia = new Lucia(adapter, {
             email: attributes.email,
             username: attributes.username,
             displayName: attributes.displayName,
+            picture: attributes.picture
         };
     }
 });

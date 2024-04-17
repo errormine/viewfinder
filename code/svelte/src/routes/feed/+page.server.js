ae3c7e648fe788beb23f2cd9a84154281cf87d73
Andrei Neacsu <aneacsu@hawk.iit.edu>
2024-04-17 18:56:53 -0500

    Fix duplicate name functions

--- code/svelte/src/routes/feed/+page.server.js
+++ code/svelte/src/routes/feed/+page.server.js
@@ -5,7 +5,7 @@ import * as db from '$lib/server/mariadb';
 export async function load({ locals }) {
     if (!locals.user) redirect(302, "/");
 
-    let photos = await db.getRecentPhotos(locals.user.id);
+    let photos = await db.getRecentUploads(locals.user.id);
     let comments = await db.getRecentComments(locals.user.id);
     let favorites = await db.getRecentFavorites(locals.user.id);
 
